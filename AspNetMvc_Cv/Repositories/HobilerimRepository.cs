d3f063356a5ab82865033ddf9f0f9d3514baf85e
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-10 21:58:39 +0300

    Repositorylerin Tanımlanması tamamlandı.

---
+++ AspNetMvc_Cv/Repositories/HobilerimRepository.cs
@@ -0,0 +1,12 @@
+﻿using AspNetMvc_Cv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvc_Cv.Repositories
+{
+    public class HobilerimRepository : GenericRepository<Tbl_Hobilerim>
+    {
+    }
+}
---
+++ AspNetMvc_Cv/Repositories/SertifikaRepository.cs
@@ -0,0 +1,12 @@
+﻿using AspNetMvc_Cv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvc_Cv.Repositories
+{
+    public class SertifikaRepository : GenericRepository<Tbl_Sertifikalarım>
+    {
+    }
+}
