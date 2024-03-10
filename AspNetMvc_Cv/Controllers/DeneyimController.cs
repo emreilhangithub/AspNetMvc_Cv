58ffdd2c4b4aa2e561560843ca9165f96d7b77ca
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-10 22:18:29 +0300

    Deneyim controller ve viewi eklendi Deneyim veri tabanından çekilerek listelendi

---
+++ AspNetMvc_Cv/Controllers/DeneyimController.cs
@@ -0,0 +1,20 @@
+﻿using AspNetMvc_Cv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc_Cv.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        // GET: Deneyim
+        DeneyimRepository repo = new DeneyimRepository();
+        public ActionResult Index()
+        {
+            var degerler = repo.List();
+            return View(degerler);
+        }
+    }
+}
