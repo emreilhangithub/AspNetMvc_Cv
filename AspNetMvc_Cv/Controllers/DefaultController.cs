ee12fbe9659eff87f93132629051b979735f9cda
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-10 18:14:48 +0300

    Entity fremwork Entity klasörü içine yeniden oluşturuldu ve controllerde entity klasörü çağırıldı.

--- AspNetMvc_Cv/Controllers/DefaultController.cs
+++ AspNetMvc_Cv/Controllers/DefaultController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using AspNetMvc_Cv.Models;
+using AspNetMvc_Cv.Models.Entity;
 
 namespace AspNetMvc_Cv.Controllers
 {
