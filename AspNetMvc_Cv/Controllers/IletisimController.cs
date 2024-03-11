2087ca80f182f23641cfe122ae8e3e9da1d9d626
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 18:16:12 +0300

    Mesajlar listelendi buna ek olarak ben mesaj silme butonu ekledim.

---
+++ AspNetMvc_Cv/Controllers/IletisimController.cs
@@ -0,0 +1,26 @@
+﻿using AspNetMvc_Cv.Models.Entity;
+using AspNetMvc_Cv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc_Cv.Controllers
+{
+    public class IletisimController : Controller
+    {
+        GenericRepository<Tbl_Iletisim> repo = new GenericRepository<Tbl_Iletisim>();
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+        public ActionResult MesajSil(int id)
+        {
+            var mesaj = repo.Find(x => x.ID == id);
+            repo.TDelete(mesaj);
+            return RedirectToAction("Index");
+        }
+    }
+}
