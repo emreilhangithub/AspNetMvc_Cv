5cb7ec58cc20c66e6f7548168d1bad763d7b8c7c
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 22:07:28 +0300

    admin listesi getirildi.

---
+++ AspNetMvc_Cv/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using AspNetMvc_Cv.Models.Entity;
+using AspNetMvc_Cv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc_Cv.Controllers
+{
+    public class AdminController : Controller
+    {
+        GenericRepository<Tbl_Admin> repo = new GenericRepository<Tbl_Admin>();
+        public ActionResult Index()
+        {
+            var liste = repo.List();
+            return View(liste);
+        }
+    }
+}
