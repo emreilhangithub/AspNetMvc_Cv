6d32f076d7c2bff26fd5a8ca026ebcf812846392
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 18:56:42 +0300

    Hakkımda bilgleri getirildi ve güncelleme işlemi yapıldı.

--- AspNetMvc_Cv/Controllers/HakkimdaController.cs
+++ AspNetMvc_Cv/Controllers/HakkimdaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AspNetMvc_Cv.Models.Entity;
+using AspNetMvc_Cv.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,28 @@ namespace AspNetMvc_Cv.Controllers
 {
     public class HakkimdaController : Controller
     {
-        // GET: Hakkimda
+        GenericRepository<Tbl_Hakkimda> repo = new GenericRepository<Tbl_Hakkimda>();
         public ActionResult Index()
         {
-            return View();
+            var hakkimda = repo.List();
+            return View(hakkimda);
         }
+
+        [HttpPost]
+        public ActionResult Index(Tbl_Hakkimda t)
+        {
+            var hakkimda = repo.Find(x => x.ID == 1); //ilkini getirdik
+            hakkimda.Ad = t.Ad;
+            hakkimda.Soyad = t.Soyad;
+            hakkimda.Adres = t.Adres;
+            hakkimda.Mail = t.Mail;
+            hakkimda.Telefon = t.Telefon;
+            hakkimda.Aciklama = t.Aciklama;
+            hakkimda.Resim = t.Resim;
+            repo.TUpdate(hakkimda);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
