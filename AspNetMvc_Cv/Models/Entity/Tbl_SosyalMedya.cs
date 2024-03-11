e6358f75ddf58e37d636cc8007cc7798130e6db8
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 20:05:01 +0300

    Sosyal medya bilgileri veri tabanından çekildi.

--- AspNetMvc_Cv/Models/Entity/Tbl_SosyalMedya.cs
+++ AspNetMvc_Cv/Models/Entity/Tbl_SosyalMedya.cs
@@ -17,5 +17,6 @@ namespace AspNetMvc_Cv.Models.Entity
         public int ID { get; set; }
         public string Ad { get; set; }
         public string Link { get; set; }
+        public string Ikon { get; set; }
     }
 }
