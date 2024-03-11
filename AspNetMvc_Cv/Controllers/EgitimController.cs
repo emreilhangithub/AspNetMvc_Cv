7dfb095c69926558490d9cceab0d11fc06bcbf45
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 15:42:15 +0300

    Eğitim silme güncelleme işlemleri tamamlandı.

--- AspNetMvc_Cv/Controllers/EgitimController.cs
+++ AspNetMvc_Cv/Controllers/EgitimController.cs
@@ -32,6 +32,35 @@ namespace AspNetMvc_Cv.Controllers
             repo.TAdd(p);
             return RedirectToAction("Index");
         }
-        
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimDuzenle(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EgitimDuzenle(Tbl_Egitimlerim t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimDuzenle");
+            }
+
+            var egitim = repo.Find(x => x.ID == t.ID);
+            egitim.Baslik = t.Baslik;
+            egitim.AltBalik1 = t.Baslik;
+            egitim.AltBaslik2 = t.Baslik;
+            egitim.Tarih = t.Tarih;
+            egitim.GNO = t.GNO;
+            repo.TUpdate(egitim);
+            return RedirectToAction("Index");
+        }
+
     }
 }
