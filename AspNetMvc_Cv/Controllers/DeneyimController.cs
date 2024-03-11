4373695bf363090bcc08b9f56700bcd406e1db9b
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 12:44:37 +0300

    Deneyim güncelleme işlemi tamamlandı.

--- AspNetMvc_Cv/Controllers/DeneyimController.cs
+++ AspNetMvc_Cv/Controllers/DeneyimController.cs
@@ -43,5 +43,16 @@ namespace AspNetMvc_Cv.Controllers
             return View(t);
         }
 
+        [HttpPost]
+        public ActionResult DeneyimGetir(Tbl_Deneyimlerim p)
+        {
+            Tbl_Deneyimlerim t = repo.Find(x => x.ID == p.ID);
+            t.Baslik = p.Baslik;
+            t.AltBaslik = p.AltBaslik;
+            t.Tarih = p.Tarih;
+            t.Aciklama  = p.Aciklama;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
     }
 }
