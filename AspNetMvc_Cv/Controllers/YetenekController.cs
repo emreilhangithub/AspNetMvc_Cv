57c442b16a2860ea57bf5b0f64b40006121b5622
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 12:57:02 +0300

    Yetenek listesi veri tabanından çekildi.

--- AspNetMvc_Cv/Controllers/YetenekController.cs
+++ AspNetMvc_Cv/Controllers/YetenekController.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using AspNetMvc_Cv.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AspNetMvc_Cv.Repositories;
 
 namespace AspNetMvc_Cv.Controllers
 {
     public class YetenekController : Controller
     {
-        // GET: Yetenek
+        GenericRepository<Tbl_Yeteneklerim> repo = new GenericRepository<Tbl_Yeteneklerim> ();
         public ActionResult Index()
         {
-            return View();
+            var yetenekler = repo.List();
+            return View(yetenekler);
         }
     }
 }
