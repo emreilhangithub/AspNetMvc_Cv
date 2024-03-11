e130871f67a8b9e098c616ecf42bfc0315b40091
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 20:12:44 +0300

    Sosyal medyalar admin panelinde listelendi.

---
+++ AspNetMvc_Cv/Controllers/SosyalMedyaController.cs
@@ -0,0 +1,20 @@
+﻿using AspNetMvc_Cv.Models.Entity;
+using AspNetMvc_Cv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc_Cv.Controllers
+{
+    public class SosyalMedyaController : Controller
+    {
+        GenericRepository<Tbl_SosyalMedya> repo = new GenericRepository<Tbl_SosyalMedya>();
+        public ActionResult Index()
+        {
+            var veriler = repo.List();
+            return View(veriler);
+        }
+    }
+}
