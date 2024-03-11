beeb87221583ec066457685238a76da255d4e54a
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 15:19:06 +0300

    Eğitim contolleri eklendi Eğitimler listelendi Eğitim ekleme sağlandı

---
+++ AspNetMvc_Cv/Controllers/EgitimController.cs
@@ -0,0 +1,32 @@
+﻿using AspNetMvc_Cv.Models.Entity;
+using AspNetMvc_Cv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc_Cv.Controllers
+{
+    public class EgitimController : Controller
+    {
+        GenericRepository<Tbl_Egitimlerim> repo = new GenericRepository<Tbl_Egitimlerim>();
+        public ActionResult Index()
+        {
+            var egitimlerim = repo.List();
+            return View(egitimlerim);
+        }
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(Tbl_Egitimlerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
