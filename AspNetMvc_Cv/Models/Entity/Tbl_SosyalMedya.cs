acecfbba418a50729394330d11102262f542ee9b
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 20:31:41 +0300

    Sosyal medyaya durum yani aktif pasiflik veri tabanında kolon ekledim.

--- AspNetMvc_Cv/Models/Entity/Tbl_SosyalMedya.cs
+++ AspNetMvc_Cv/Models/Entity/Tbl_SosyalMedya.cs
@@ -18,5 +18,6 @@ namespace AspNetMvc_Cv.Models.Entity
         public string Ad { get; set; }
         public string Link { get; set; }
         public string Ikon { get; set; }
+        public Nullable<bool> Durum { get; set; }
     }
 }
