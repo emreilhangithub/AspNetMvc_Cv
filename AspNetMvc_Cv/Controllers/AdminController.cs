b19ca55b2ce7ed9b96f5e8172625fb8d0bc6867d
emreilhangithub <mustafaemreilhan1903@gmail.com>
2024-03-11 22:15:46 +0300

    Admin düzenle Admin ekle Admin sil sayfaları eklendi.

--- AspNetMvc_Cv/Controllers/AdminController.cs
+++ AspNetMvc_Cv/Controllers/AdminController.cs
@@ -16,5 +16,38 @@ namespace AspNetMvc_Cv.Controllers
             var liste = repo.List();
             return View(liste);
         }
+        [HttpGet]
+        public ActionResult AdminEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AdminEkle(Tbl_Admin p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult AdminSil(int id)
+        {
+            var admin = repo.Find(x => x.ID == id);
+            repo.TDelete(admin);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult AdminDuzenle(int id)
+        {
+            var admin = repo.Find(x => x.ID == id);
+            return View(admin);
+        }
+
+        [HttpPost]
+        public ActionResult AdminDuzenle(Tbl_Admin t)
+        {
+            var admin = repo.Find(x => x.ID == t.ID);
+            admin.KullaniciAdi = t.KullaniciAdi;
+            admin.Sifre = t.Sifre;
+            repo.TUpdate(admin);
+            return RedirectToAction("Index");
+        }
     }
 }
